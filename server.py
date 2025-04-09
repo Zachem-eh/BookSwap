2e6e39ac2c9d976baa7ff67ec6d6baa63df7630f
Zachem-eh <vladsasha7002@gmail.com>
2025-04-09 21:42:59 +0300

    add prototype register and login class

--- server.py
+++ server.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.simple import EmailField, BooleanField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Ваш email')
+    password = PasswordField('Пароль')
+    repeat_password = PasswordField('Подтвердите пароль')
+    surname = StringField('Фамилия')
+    name = StringField('Имя')
+    age = StringField('Возраст')
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 @app.route('/')
 def home():
