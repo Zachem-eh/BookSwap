f07dd432cc61d8dd5e3585a9b89d4677753bb609
yuliapchr <ulapechurenko@gmail.com>
2025-04-09 16:53:33 +0300

    create home commit

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def main_page():
-    return '<h1>BookSwap</h1>'
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
