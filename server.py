2fd1c481a61e469f2a531f59847864b0001d7b78
Zachem-eh <vladsasha7002@gmail.com>
2025-04-14 16:04:15 +0300

    add register without db

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.fields.simple import EmailField, BooleanField
 from wtforms.validators import DataRequired
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum'
 
 
 class RegisterForm(FlaskForm):
@@ -27,6 +29,23 @@ class LoginForm(FlaskForm):
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # sess = db_session.create_session()
+        # user = User()
+        # user.email = form.email.data
+        # user.password = form.password.data
+        # user.surname = form.surname.data
+        # user.name = form.name.data
+        # user.age = int(form.age.data)
+        # sess.add(user)
+        # sess.commit()
+        return redirect('/')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
