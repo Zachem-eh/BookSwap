4a6e7bf67e4dd24be7457c54d3bd80a6a9986681
yuliapchr <ulapechurenko@gmail.com>
2025-04-21 21:15:36 +0300

    add defbooks

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.fields.simple import EmailField, BooleanField
@@ -37,6 +37,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
 
 class BookForm(FlaskForm):
     title = StringField('Название', validators=[DataRequired()])
@@ -81,6 +82,42 @@ def register():
         sess.commit()
         return redirect('/')
     return render_template('register.html', form=form)
+
+
+@app.route('/books')
+@login_required
+def books():
+    db_sess = db_session.create_session()
+    genre_filter = request.args.get('genre')
+
+    if genre_filter:
+        all_books = db_sess.query(Book).filter(Book.genre == genre_filter).all()
+    else:
+        all_books = db_sess.query(Book).all()
+
+    genres = db_sess.query(Book.genre).distinct().all()
+    genres = [g[0] for g in genres]
+
+    return render_template('books.html', books=all_books, genres=genres, selected_genre=genre_filter)
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+@login_required
+def add_book():
+    form = BookForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        book = Book(
+            title=form.title.data,
+            author=form.author.data,
+            genre=form.genre.data,
+            age=form.age.data,
+            user=current_user
+        )
+        db_sess.add(book)
+        db_sess.commit()
+        return redirect('/books')
+    return render_template('add_book.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
